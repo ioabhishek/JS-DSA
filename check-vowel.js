503966d6fa1f8868835446a920ab996c24b6cb13
Abhishek Sharma <akstenx@gmail.com>
2025-09-20 10:54:20 +0530

    Add various JavaScript programs for number and string manipulations

--- check-vowel.js
+++
@@ -1,5 +0,0 @@
-// Write a function that takes a string as input and returns the number of words in the sentence that start with a vowel. The programme should use a function to perform the calculation
-
-// 1. convert in array 
-// 2. loop to check weather it starts with vowel or not
-let str  = "this is such a beautiful place in india"
